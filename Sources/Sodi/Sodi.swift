3dc390f7081ad57159eb73cd433c67dff99f2a4e
Alexander <sphc@yandex.ru>
2022-04-12 22:03:26 +0300

    code refactoring

--- Sources/Sodi/Sodi.swift
+++ Sources/Sodi/Sodi.swift
@@ -8,7 +8,7 @@
 import Foundation
 #endif
 
-final class Sodi : SodiStorage, ISodi {
+final class Sodi : SodiStorage {
     
     lazy var storage: Storage = Storage()
     
@@ -42,4 +42,41 @@ final class Sodi : SodiStorage, ISodi {
     static func hasInstance(tagWrapper: SodiTagWrapper) -> Bool {
         return instance.hasInstance(tagWrapper: tagWrapper)
     }
+}
+
+func bindSingle<T>(to: Any? = nil, creater: @escaping LambdaWithReturn<T>) {
+    let tagWrapper = createTagWrapper(toTag: to, name: "\(T.self)")
+    let holder = SodiSingle(tagWrapper: tagWrapper, creator: creater)
+    Sodi.insertHolder(tagWrapper: tagWrapper, sodiHolder: holder)
+}
+
+func bindProvider<T>(to: Any? = nil, creater: @escaping LambdaWithReturn<T>) {
+    let tagWrapper = createTagWrapper(toTag: to, name: "\(T.self)")
+    let holder = SodiProvider(tagWrapper: tagWrapper, creator: creater)
+    Sodi.insertHolder(tagWrapper: tagWrapper, sodiHolder: holder)
+}
+
+func unbind<T: Any>(from: Any? = nil) -> T? {
+    let tagWrapper = createTagWrapper(toTag: from, name: "\(T.self)")
+    var holder = Sodi.deleteHolder(tagWrapper: tagWrapper)
+    return holder.getInstance() as? T ?? nil
+}
+
+func hasInstance(from: Any) -> Bool {
+    let tagWrapper = createTagWrapper(toTag: from, name: "\(from.self)")
+    return Sodi.hasInstance(tagWrapper: tagWrapper)
+}
+
+func instance<T: Any>(from: Any? = nil) -> T {
+    let tagWrapper = createTagWrapper(toTag: from, name: "\(T.self)")
+    var holder = Sodi.selectHolder(tagWrapper: tagWrapper)
+    return try! holder.getHolderValue()
+}
+
+private func createTagWrapper(toTag: Any? = nil, name: String) -> SodiTagWrapper {
+    var tagName: String = name
+    if let obj = toTag {
+        tagName = "\(obj.self)"
+    }
+    return SodiTagWrapper(tagName: tagName)
 }
